661cf48d196d07eebd6e29cf389be1faafc8ff42
volodymyr.mishchuk <pidsercem@gmail.com>
2025-06-16 15:22:50 +0200

    edit 001-create-schema.sql

--- src/main/resources/db/changelog/001-create-schema.sql
+++ src/main/resources/db/changelog/001-create-schema.sql
@@ -2,45 +2,46 @@
 
 --changeset volodymyr.mishchuk:1
 
-CREATE TABLE Persons (
-    id UUID PRIMARY KEY,
-    first_name VARCHAR(50) NOT NULL,
-    last_name VARCHAR(50) NOT NULL,
-    phone_number VARCHAR(16) NOT NULL UNIQUE,
-    email VARCHAR(255) NOT NULL UNIQUE,
-    password VARCHAR(255),
-    role VARCHAR(15) NOT NULL,
-    status VARCHAR(10) NOT NULL,
-    created_at TIMESTAMPTZ NOT NULL,
-    updated_at TIMESTAMPTZ
+CREATE TABLE "users" (
+    "id" UUID PRIMARY KEY,
+    "first_name" VARCHAR(50) NOT NULL,
+    "last_name" VARCHAR(50) NOT NULL,
+    "birth_day" TIMESTAMPTZ NOT NULL,
+    "phone_number" VARCHAR(16) NOT NULL UNIQUE,
+    "email" VARCHAR(255) NOT NULL UNIQUE,
+    "password" VARCHAR(255),
+    "role" VARCHAR(15) NOT NULL,
+    "status" VARCHAR(10) NOT NULL,
+    "created_at" TIMESTAMPTZ NOT NULL,
+    "updated_at" TIMESTAMPTZ
 );
 
-CREATE TABLE Accounts (
-    id UUID PRIMARY KEY,
-    number BIGINT NOT NULL UNIQUE,
-    status VARCHAR(10) NOT NULL,
-    balance MONEY NOT NULL NOT NULL DEFAULT 0::money CHECK (balance >= 0::money),
-    currency CHAR(3) NOT NULL,
-    token_MetaTradeAPI TEXT NOT NULL,
-    created_at TIMESTAMPTZ NOT NULL,
-    updated_at TIMESTAMPTZ,
-    person_id UUID NOT NULL REFERENCES Persons(id)
+CREATE TABLE "accounts" (
+    "id" UUID PRIMARY KEY,
+    "number" BIGINT NOT NULL UNIQUE,
+    "status" VARCHAR(10) NOT NULL,
+    "balance" MONEY NOT NULL NOT NULL DEFAULT 0::money CHECK (balance >= 0::money),
+    "currency" CHAR(3) NOT NULL,
+    "token_MetaTradeAPI" TEXT NOT NULL,
+    "created_at" TIMESTAMPTZ NOT NULL,
+    "updated_at" TIMESTAMPTZ,
+    "user_id" UUID NOT NULL REFERENCES Persons(id)
 );
 
-CREATE TABLE Sources (
-    id UUID PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    platform VARCHAR(50) NOT NULL,
-    token TEXT NOT NULL,
-    created_at TIMESTAMPTZ NOT NULL,
-    updated_at TIMESTAMPTZ
+CREATE TABLE "sources" (
+    "id" UUID PRIMARY KEY,
+    "name" VARCHAR(255) NOT NULL,
+    "platform" VARCHAR(50) NOT NULL,
+    "token" TEXT NOT NULL,
+    "created_at" TIMESTAMPTZ NOT NULL,
+    "updated_at" TIMESTAMPTZ
 );
 
-CREATE TABLE Transactions (
-    id UUID PRIMARY KEY,
-    amount MONEY NOT NULL DEFAULT 0::money CHECK (amount >= 0::money),
-    direction VARCHAR(15) NOT NULL,
-    created_at TIMESTAMPTZ NOT NULL,
-    account_id UUID NOT NULL REFERENCES Accounts(id),
-    source_id UUID NOT NULL REFERENCES Sources(id)
+CREATE TABLE "transactions" (
+    "id" UUID PRIMARY KEY,
+    "amount" MONEY NOT NULL DEFAULT 0::money CHECK (amount >= 0::money),
+    "direction" VARCHAR(15) NOT NULL,
+    "created_at" TIMESTAMPTZ NOT NULL,
+    "account_id" UUID NOT NULL REFERENCES Accounts(id),
+    "source_id" UUID NOT NULL REFERENCES Sources(id)
 );
