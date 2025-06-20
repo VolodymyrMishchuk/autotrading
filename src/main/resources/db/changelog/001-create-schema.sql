ff9399898f1d68950145c1cfb4a527269e91d3ed
VolodymyrMishchuk <pidsercem@gmail.com>
2025-06-20 18:50:50 +0200

    change db schema

--- src/main/resources/db/changelog/001-create-schema.sql
+++ src/main/resources/db/changelog/001-create-schema.sql
@@ -6,7 +6,7 @@ CREATE TABLE "users" (
     "id" UUID PRIMARY KEY,
     "first_name" VARCHAR(50) NOT NULL,
     "last_name" VARCHAR(50) NOT NULL,
-    "birth_day" TIMESTAMPTZ NOT NULL,
+    "birth_day" DATE NOT NULL,
     "phone_number" VARCHAR(16) NOT NULL UNIQUE,
     "email" VARCHAR(255) NOT NULL UNIQUE,
     "password" VARCHAR(255),
@@ -20,12 +20,12 @@ CREATE TABLE "accounts" (
     "id" UUID PRIMARY KEY,
     "number" BIGINT NOT NULL UNIQUE,
     "status" VARCHAR(10) NOT NULL,
-    "balance" MONEY NOT NULL NOT NULL DEFAULT 0::money CHECK (balance >= 0::money),
+    "balance" NUMERIC(15,2) NOT NULL DEFAULT 0::money CHECK (balance >= 0::money),
     "currency" CHAR(3) NOT NULL,
     "token_MetaTradeAPI" TEXT NOT NULL,
     "created_at" TIMESTAMPTZ NOT NULL,
     "updated_at" TIMESTAMPTZ,
-    "user_id" UUID NOT NULL REFERENCES Persons(id)
+    "user_id" UUID NOT NULL REFERENCES users(id)
 );
 
 CREATE TABLE "sources" (
@@ -40,7 +40,7 @@ CREATE TABLE "sources" (
 
 CREATE TABLE "transactions" (
     "id" UUID PRIMARY KEY,
-    "amount" MONEY NOT NULL DEFAULT 0::money CHECK (amount >= 0::money),
+    "amount" NUMERIC(15,2) NOT NULL DEFAULT 0::money CHECK (amount >= 0::money),
     "direction" VARCHAR(15) NOT NULL,
     "created_at" TIMESTAMPTZ NOT NULL,
     "account_id" UUID NOT NULL REFERENCES Accounts(id),
