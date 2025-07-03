f67888d3d8e5693e4f66a017d9f43aa063cdd712
VolodymyrMishchuk <pidsercem@gmail.com>
2025-07-03 13:57:04 +0200

    add UserRole.java

---
+++ src/main/java/com/mishchuk/autotrade/service/model/UserRole.java
@@ -0,0 +1,21 @@
+package com.mishchuk.autotrade.service.model;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum UserRole {
+
+    ROLE_CUSTOMER(UserRoleConstants.CUSTOMER),
+    ROLE_ADMIN(UserRoleConstants.ADMIN),
+    ROLE_SUPER_ADMIN(UserRoleConstants.SUPER_ADMIN);
+
+    private final String value;
+
+    public static class UserRoleConstants {
+        public static final String CUSTOMER = "ROLE_CUSTOMER";
+        public static final String ADMIN = "ROLE_ADMIN";
+        public static final String SUPER_ADMIN = "ROLE_SUPER_ADMIN";
+    }
+}
