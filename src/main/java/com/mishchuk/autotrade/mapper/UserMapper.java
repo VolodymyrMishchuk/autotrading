74f7c473a32af52add1975fded2b2ff58c3faed1
VolodymyrMishchuk <pidsercem@gmail.com>
2025-07-25 10:36:46 +0200

    edit UserMapper.java

--- src/main/java/com/mishchuk/autotrade/mapper/UserMapper.java
+++ src/main/java/com/mishchuk/autotrade/mapper/UserMapper.java
@@ -10,76 +10,136 @@ import java.util.UUID;
 @Component
 public class UserMapper {
 
-    public UserDetailDto toUserDetailDto(User user) {
-        return UserDetailDto.builder()
-                .id(user.getId())
-                .firstName(user.getFirstName())
-                .lastName(user.getLastName())
-                .email(user.getEmail())
-                .phoneNumber(user.getPhoneNumber())
-                .birthDate(user.getBirthDate())
-                .createdAt(user.getCreatedAt())
-                .updatedAt(user.getUpdatedAt())
-                .role(Role.valueOf(user.getRole()))
-                .status(Status.valueOf(user.getStatus()))
-                .build();
-    }
-
-    public User toUser(UserCreateDto dto) {
-        return User.builder()
-                .firstName(dto.getFirstName())
-                .lastName(dto.getLastName())
-                .birthDate(dto.getBirthDate())
-                .phoneNumber(dto.getPhoneNumber())
-                .email(dto.getEmail())
-                .build();
-    }
-
-    public User toUser(UserUpdateDto dto) {
-        return User.builder()
-                .id(dto.getId())
-                .firstName(dto.getFirstName())
-                .lastName(dto.getLastName())
-                .birthDate(dto.getBirthDate())
-                .phoneNumber(dto.getPhoneNumber())
-                .email(dto.getEmail())
-                .password(dto.getPassword())
-                .role(String.valueOf(dto.getRole()))
-                .status(String.valueOf(dto.getStatus()))
-                .build();
-    }
-
-    public User toUser(UserCompleteRegistrationDto dto) {
-        return User.builder()
-                .token(dto.getToken())
-                .password(dto.getPassword())
-                .build();
-    }
-
     public User toUser(UserEntity entity) {
         return User.builder()
-                .id(UUID.entity.getId())
+                .id(entity.getId() != null ? entity.getId().toString() : null)
                 .firstName(entity.getFirstName())
                 .lastName(entity.getLastName())
                 .birthDate(entity.getBirthDay())
                 .phoneNumber(entity.getPhoneNumber())
                 .email(entity.getEmail())
-                .role(String.valueOf(entity.getRole()))
-                .status(String.valueOf(entity.getStatus()))
+                .password(entity.getPassword())
+                .role(entity.getRole())
+                .status(entity.getStatus())
                 .createdAt(entity.getCreatedAt())
                 .updatedAt(entity.getUpdatedAt())
+                .token(entity.getToken()) // чи тре тут токен?
                 .build();
     }
 
     public UserEntity toUserEntity(User user) {
         return UserEntity.builder()
-                .id(user.getId() != null ? user.getId() : null)
+                .id(user.getId() != null ? UUID.fromString(user.getId()) : null)
                 .firstName(user.getFirstName())
                 .lastName(user.getLastName())
                 .birthDay(user.getBirthDate())
                 .phoneNumber(user.getPhoneNumber())
                 .email(user.getEmail())
-                .password(user.getPassword())
+                .password(user.getPassword()) // знову ж таки питання, чи доречне тут це поле?
+                .role(user.getRole())
+                .status(user.getStatus())
+                .createdAt(user.getCreatedAt())
+                .updatedAt(user.getUpdatedAt())
+                .token(user.getToken()) // ???
+                .build();
+    }
+
+    public User toUser(UserRegistrationDto dto) {
+        return User.builder()
+                .firstName(dto.getFirstName())
+                .lastName(dto.getLastName())
+                .birthDate(dto.getBirthDate())
+                .phoneNumber(dto.getPhoneNumber())
+                .email(dto.getEmail())
+                .password(dto.getPassword())
+                .build();
+    }
+
+    public User toUser(UserUpdateDto dto) {
+        return User.builder()
+                .firstName(dto.getFirstName())
+                .lastName(dto.getLastName())
+                .birthDate(dto.getBirthDate())
+                .build();
+    }
+
+    public User toUser(UserCompleteRegistrationDto dto) {
+        return User.builder()
+                .token(UUID.fromString(dto.getConfirmationToken()))
+                .build();
+    }
+
+    public User toUser(EmailChangeDto dto) {
+        return User.builder()
+                .email(dto.getNewEmail())
+                .build();
+    }
+
+    public User toUser(EmailResetRequestDto dto) {
+        return User.builder()
+                .phoneNumber(dto.getPhoneNumber())
+                .build();
+    }
+
+    public User toUser(EmailResetCompleteDto dto) {
+        return User.builder()
+                .token(UUID.fromString(dto.getToken()))
+                .email(dto.getEmail())
+                .build();
+    }
+
+    public User toDomain(PasswordChangeDto dto) {
+        return User.builder()
+                .password(dto.getNewPassword())
+                .build();
+    }
+
+    public User toDomain(PasswordResetRequestDto dto) {
+        return User.builder()
+                .email(dto.getEmail())
+                .build();
+    }
+
+    public User toDomain(PasswordResetCompleteDto dto) {
+        return User.builder()
+                .token(UUID.fromString(dto.getToken()))
+                .password(dto.getPassword())
+                .build();
+    }
+
+    public User toDomain(PhoneNumberChangeDto dto) {
+        return User.builder()
+                .phoneNumber(dto.getNewPhoneNumber())
+                .build();
+    }
+
+    public User toDomain(PhoneNumberResetRequestDto dto) {
+        return User.builder()
+                .email(dto.getEmail())
+                .build();
+    }
+
+    public User toDomain(PhoneNumberResetCompleteDto dto) {
+        return User.builder()
+                .token(UUID.fromString(dto.getToken()))
+                .phoneNumber(dto.getPhoneNumber())
+                .build();
+    }
+
+    public User toDomain(RoleChangeDto dto) {
+        return User.builder()
+                .role(dto.getNewRole())
+                .build();
+    }
+
+    public UserDetailDto toUserDetailDto(User user) {
+        return UserDetailDto.builder()
+                .id(UUID.fromString(user.getId()))
+                .firstName(user.getFirstName())
+                .lastName(user.getLastName())
+                .email(user.getEmail())
+                .phoneNumber(user.getPhoneNumber())
+                .birthDate(user.getBirthDate())
                 .role(user.getRole())
                 .status(user.getStatus())
                 .createdAt(user.getCreatedAt())
