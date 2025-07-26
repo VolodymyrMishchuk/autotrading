70ef9a171c6c3b309cdf060dbe0598e45317be91
VolodymyrMishchuk <pidsercem@gmail.com>
2025-07-26 23:15:05 +0200

    add CabinetSourceEntity.java and CabinetSourceRepository.java

---
+++ src/main/java/com/mishchuk/autotrade/repository/entity/CabinetSourceEntity.java
@@ -0,0 +1,31 @@
+package com.mishchuk.autotrade.repository.entity;
+
+import com.mishchuk.autotrade.enums.Status;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "cabinet_sources")
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class CabinetSourceEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "cabinet_id", nullable = false)
+    private CabinetEntity cabinet;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "source_id", nullable = false)
+    private SourceEntity source;
+
+    private Status status;
+}
