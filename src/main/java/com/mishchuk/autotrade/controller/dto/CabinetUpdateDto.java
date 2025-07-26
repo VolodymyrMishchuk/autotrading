931ff1b8d5d03a3d05472f7eb71e2e3f8f5c8d32
VolodymyrMishchuk <pidsercem@gmail.com>
2025-07-26 18:46:47 +0200

    add Cabinet's DTOs

---
+++ src/main/java/com/mishchuk/autotrade/controller/dto/CabinetUpdateDto.java
@@ -0,0 +1,18 @@
+package com.mishchuk.autotrade.controller.dto;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.Builder;
+import lombok.Data;
+import java.util.List;
+import java.util.UUID;
+
+@Data
+@Builder
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class CabinetUpdateDto {
+    private String name;
+    private String metaTradeToken;
+    private Status status;
+    private List<UUID> sourceIds;
+}
